d6c65b47b982f7c577c8f346561ecb0ce9f4ef23
Ferdinand Eiteneuer <ferdinand.eiteneuer@damedic.ai>
2024-12-28 00:29:44 +0100

    use git reset --hard origin/master instead of git pull to keep repository up to date (in case I manually make changes i dont want the merge to fail)

--- src/forro_festivals/scripts/reload-all.sh
+++ src/forro_festivals/scripts/reload-all.sh
@@ -2,14 +2,8 @@
 
 cd /home/feiteneuer/forro-festivals
 
-git pull
+git reset --hard origin/master
 
-# This is supposed to reload the app since pythonanywhere is checking for file changes
+# This is reloads the app since pythonanywhere is checking for file changes
+# to the wsgi file and reloads the app upon last changed
 touch /var/www/www_forro-festivals_com_wsgi.py
-
-
-
-# Make the API call to reload the web app
-#curl -X POST \
-#    -H "Authorization: Token $API_TOKEN" \
-#    https://www.pythonanywhere.com/api/v0/user/$USERNAME/webapps/www.forro-festivals.com/reload/
