f9f613f2e7a962ed793430b44fc5e88d25a6391d
Ferdinand Eiteneuer <ferdinand.eiteneuer@rwth-aachen.de>
2025-01-04 22:03:20 +0100

    add several utility functions into event for creating events from request objects and some repr like function for html display

--- src/forro_festivals/scripts/event.py
+++ src/forro_festivals/scripts/event.py
@@ -1,15 +1,20 @@
 """
 Defines the basic object to hold the information about a Forro Festival
 """
-
-from pydantic import BaseModel, Field, field_validator, model_validator
+import pydantic
+from pydantic import BaseModel, Field, field_validator, model_validator, ValidationError
 import re
 from datetime import datetime
+from urllib.parse import urlparse
 
-from forro_festivals.config import date_fmt_ymd_hms, date_fmt_ymd
+from forro_festivals.config import DateFormats
 
 def get_timestamp():
-    return datetime.now().strftime(date_fmt_ymd_hms)
+    return datetime.now().strftime(DateFormats.ymd_hms)
+
+def transform_date(date, fmt_from, fmt_to):
+    return datetime.strptime(date, fmt_from).strftime(fmt_to)
+
 
 class Event(BaseModel):
     # Note: potentially, I can add a date for the start of the ticket sell
@@ -27,11 +32,11 @@ class Event(BaseModel):
 
     @property
     def start(self):
-        return datetime.strptime(self.date_start, date_fmt_ymd)
+        return datetime.strptime(self.date_start, DateFormats.ymd)
 
     @property
     def end(self):
-        return datetime.strptime(self.date_end, date_fmt_ymd)
+        return datetime.strptime(self.date_end, DateFormats.ymd)
 
     @field_validator('date_start', 'date_end')
     def validate_start_end_dates(cls, value):
@@ -49,3 +54,66 @@ class Event(BaseModel):
 
     def to_tuple(self):
         return tuple(self.model_dump().values())
+
+    def to_html_string(self):
+        link = self.link
+        if urlparse(self.link).scheme == '':
+            link = f'http://{link}'
+        start = transform_date(self.date_start, fmt_from=DateFormats.ymd, fmt_to=DateFormats.dm)
+        end = transform_date(self.date_end, fmt_from=DateFormats.ymd, fmt_to=DateFormats.dm)
+        return f"[{self.city}, {self.country}] {start} \u2013 {end} | <a href='{link}'>{self.link_text}</a>"
+
+    @classmethod
+    def from_request(cls, request):
+        default_kwargs = dict(
+            organizer='None',
+            validated=False,
+            source='add-festival',
+        )
+        kwargs = {
+            key: value
+            for key, value in request.form.items()
+            if value != ''
+        }
+        kwargs = {**default_kwargs, **kwargs}
+        return Event(**kwargs)
+
+    @classmethod
+    def get_default_event(cls):
+        now = datetime.now().strftime(DateFormats.ymd)
+        return cls(
+            date_start=now,
+            date_end=now,
+            city='City',
+            country='Country',
+            organizer='None',
+            link='https://www.example.com',
+            link_text='Event Name',
+            validated=False,
+            source='add-festival',
+        )
+
+    @classmethod
+    def human_readable_validation_error_explanation(cls, exc: pydantic.ValidationError):
+        try:
+            N_err = exc.error_count()
+            error_or_errors = "error" if N_err == 1 else "errors"
+            error_msg = f'Unfortunately, the data you submitted contained {N_err} {error_or_errors} :<br><ul>'
+            translation_dict = {
+                'link': 'Link',
+                'link_text': 'Festival',
+                'country': 'Country',
+                'date_end': 'End Date',
+                'date_start': 'Start Date',
+                'city': 'City',
+            }
+            for err in exc.errors():
+                if err['type'] == 'missing':
+                    field = translation_dict[err['loc'][0]]
+                    error_msg += f'<li>Missing Field: {field}</li>'
+                elif err['type'] == 'value_error':
+                    error_msg += f'<li>{err["msg"]}</li>'
+            error_msg += '</ul>'
+            return error_msg
+        except Exception:
+            return f'Error: {exc.errors()}'
