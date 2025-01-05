20fc4f3c8e8fe53212898d812de4f33bb9c7e3f5
Ferdinand Eiteneuer <ferdinand.eiteneuer@rwth-aachen.de>
2025-01-05 23:34:03 +0100

    add ff command line tool to make common tasks like rendering html pages or db operations very easy

---
+++ src/forro_festivals/scripts/__main__.py
@@ -0,0 +1,80 @@
+import os
+import subprocess
+
+import click
+
+from forro_festivals.scripts.create_festivals_html import create_festivals_html
+from forro_festivals.scripts.create_impressum_html import create_impressum_html
+from forro_festivals.scripts.render_html_pages import render_html_pages
+from forro_festivals.scripts.db import backup_db, delete_events_by_ids
+from forro_festivals.scripts.reload_app import reload_app_by_touch
+from forro_festivals.scripts.update_db_with_forro_app import update_db_with_forro_app
+
+
+@click.group()
+def ff():
+    """Main entry point for Forro-Festivals."""
+    pass
+
+
+@ff.command()
+def reload_app():
+    """Reloads the app."""
+    reload_app_by_touch()
+
+@ff.command()
+def query_forro_app_update_db():
+    """Queries festival data from forro-app.com and saves it into the database"""
+    update_db_with_forro_app()
+
+
+############
+# DATABASE #
+############
+@click.group()
+def db():
+    """Database related commands."""
+    pass
+
+# Command: db delete
+@db.command()
+@click.option('--ids', required=True, type=str, help='ID(s) of the record to delete. Example: "1,5,10-19,30"')
+def delete(ids):
+    """Delete record(s) from the database."""
+    delete_events_by_ids(ids)
+
+# Command: db backup
+@db.command()
+def backup():
+    """Create a backup of the database."""
+    backup_db()
+
+########
+# HTML #
+########
+# Command: render html pages
+@click.group()
+def html():
+    """Render HTML pages."""
+
+@html.command()
+def impressum():
+    """Renders the impressum.html from its template"""
+    create_impressum_html()
+
+@html.command()
+def festivals():
+    """Renders the festivals.html from its template and the db"""
+    create_festivals_html()
+
+@html.command()
+def all():
+    """Recreates all static html pages"""
+    render_html_pages()
+
+
+ff.add_command(db)
+ff.add_command(html)
+
+if __name__ == '__main__':
+    ff()
