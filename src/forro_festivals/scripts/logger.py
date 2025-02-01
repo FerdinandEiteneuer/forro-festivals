bc0cb06607b33008593c2520668f1bd26b0e478c
Ferdinand Eiteneuer <ferdinand.eiteneuer@damedic.ai>
2025-02-02 00:23:13 +0100

    logger more configurable

--- src/forro_festivals/scripts/logger.py
+++ src/forro_festivals/scripts/logger.py
@@ -1,8 +1,21 @@
 import logging
 from logging.handlers import RotatingFileHandler
+from pathlib import Path
 
 from forro_festivals.scripts.notification import post_error_to_ntfy_channel
 from forro_festivals.config import LOG_CONSOLE, LOG_FOLDER, NTFY_TOPIC
+
+
+class NtfyHandler(logging.Handler):
+    """Sends error messages to ntfy."""
+    def __init__(self, formatter, *args, **kwargs):
+        self.formatter = formatter
+        super().__init__(*args, **kwargs)
+
+    def emit(self, record):
+        if record.levelno >= 40 and NTFY_TOPIC is not None:
+            ntfy_msg = self.formatter.format(record)
+            post_error_to_ntfy_channel(message=ntfy_msg, topic=NTFY_TOPIC)
 
 def get_logger():
     logger = logging.getLogger('app')
@@ -20,19 +33,15 @@ def get_logger():
         console_handler.setFormatter(formatter)
         logger.addHandler(console_handler)
 
-    file_handler = RotatingFileHandler(filename=LOG_FOLDER / 'app.log', maxBytes=1e6, backupCount=5)
-    file_handler.setLevel(logging.INFO)
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
+    if LOG_FOLDER:
+        filename = Path(LOG_FOLDER) / 'app.log'
+        file_handler = RotatingFileHandler(filename=filename, maxBytes=1e6, backupCount=5)
+        file_handler.setLevel(logging.INFO)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
 
-    class NtfyHandler(logging.Handler):
-        """Sends error messages to ntfy."""
-        def emit(self, record):
-            if record.levelno >= 40 and NTFY_TOPIC is not None:
-                ntfy_msg = formatter.format(record)
-                post_error_to_ntfy_channel(message=ntfy_msg, topic=NTFY_TOPIC)
-
-    logger.addHandler(NtfyHandler())
+    if NTFY_TOPIC:
+        logger.addHandler(NtfyHandler(formatter=formatter))
 
     return logger
 
