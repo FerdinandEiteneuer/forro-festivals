73a62c140353c1e1183b60d4bde814bcb52f00aa
Ferdinand Eiteneuer <ferdinand.eiteneuer@damedic.ai>
2025-02-03 23:25:28 +0100

    make "env" accessible in templates / jinja

--- src/forro_festivals/app.py
+++ src/forro_festivals/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from forro_festivals.config import APP_SECRET_KEY
+from forro_festivals.config import APP_SECRET_KEY, ENV
 from forro_festivals.routes import blueprints, auth
 
 
@@ -10,6 +10,11 @@ def build_app():
 
     for blueprint in blueprints:
         app.register_blueprint(blueprint)
+
+    @app.context_processor
+    def inject_env():
+        # This will make env accessible in the html templates via jinja
+        return {"env": ENV}
 
     auth.login_manager.init_app(app)
     return app
